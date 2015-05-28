7897fcb937a6abb37566754daee021fd29e00894
Abhishek Batra <abhishekbatra1993@gmail.com>
2015-05-29 02:32:05 +0530

    Delete and Edit method in Quiz Adapter

--- source-code/app/src/main/java/org/buildmlearn/toolkit/templates/QuizAdapter.java
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/templates/QuizAdapter.java
@@ -5,7 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import org.buildmlearn.toolkit.R;
 import org.buildmlearn.toolkit.views.TextViewPlus;
@@ -59,6 +66,8 @@ public class QuizAdapter extends BaseAdapter {
             holder.options.add((TextViewPlus) convertView.findViewById(R.id.answer4));
             holder.questionIcon = (TextViewPlus) convertView.findViewById(R.id.question_icon);
             holder.quizOptionsBox = (LinearLayout) convertView.findViewById(R.id.quiz_options_box);
+            holder.delete = (Button) convertView.findViewById(R.id.quiz_item_delete);
+            holder.edit = (Button) convertView.findViewById(R.id.quiz_item_edit);
 
         } else {
             holder = (Holder) convertView.getTag();
@@ -103,8 +112,137 @@ public class QuizAdapter extends BaseAdapter {
                 notifyDataSetChanged();
             }
         });
+
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editItem(position, context);
+            }
+        });
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quizData.remove(position);
+                notifyDataSetChanged();
+            }
+        });
         convertView.setTag(holder);
         return convertView;
+    }
+
+    private void editItem(final int position, final Context context) {
+        QuizModel data = getItem(position);
+
+        boolean wrapInScrollView = true;
+        final MaterialDialog dialog = new MaterialDialog.Builder(context)
+                .title(R.string.quiz_edit)
+                .customView(R.layout.quiz_dialog_add_question, wrapInScrollView)
+                .positiveText(R.string.quiz_add)
+                .negativeText(R.string.quiz_delete)
+                .build();
+
+        final EditText question = (EditText) dialog.findViewById(R.id.quiz_question);
+        final ArrayList<RadioButton> buttons = new ArrayList<>();
+        final ArrayList<EditText> options = new ArrayList<>();
+        options.add((EditText) dialog.findViewById(R.id.quiz_option_1));
+        options.add((EditText) dialog.findViewById(R.id.quiz_option_2));
+        options.add((EditText) dialog.findViewById(R.id.quiz_option_3));
+        options.add((EditText) dialog.findViewById(R.id.quiz_option_4));
+        buttons.add((RadioButton) dialog.findViewById(R.id.quiz_radio_1));
+        buttons.add((RadioButton) dialog.findViewById(R.id.quiz_radio_2));
+        buttons.add((RadioButton) dialog.findViewById(R.id.quiz_radio_3));
+        buttons.add((RadioButton) dialog.findViewById(R.id.quiz_radio_4));
+
+        for(int i=0; i<data.getOptions().size(); i++) {
+            options.get(i).setText(data.getOptions().get(i));
+        }
+
+        question.setText(data.getQuestion());
+        buttons.get(data.getCorrectAnswer()).setChecked(true);
+
+        for (final RadioButton button : buttons) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    checkButton(buttons, options, button.getId(), context);
+                }
+            });
+        }
+
+        dialog.getActionButton(DialogAction.POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                boolean isValidated = true;
+                int checkedAns = getCheckedAnswer(buttons);
+                if (checkedAns < 0) {
+                    Toast.makeText(context, "Choose a correct option", Toast.LENGTH_SHORT).show();
+                    isValidated = false;
+                }
+                if (question.getText().toString().equals("")) {
+
+                    question.setError("Question is required");
+                    isValidated = false;
+                }
+
+                int optionCount = 0;
+                for (EditText option : options) {
+                    if (!option.getText().toString().equals("")) {
+                        optionCount++;
+                    }
+                }
+                if (optionCount < 2) {
+                    Toast.makeText(context, "Minimum two multiple answers are required.", Toast.LENGTH_SHORT).show();
+                    isValidated = false;
+                }
+
+                if (isValidated) {
+                    dialog.dismiss();
+                    ArrayList<String> answerOptions = new ArrayList<String>();
+                    int correctAnswer = 0;
+                    for (int i = 0; i < buttons.size(); i++) {
+                        if (buttons.get(i).isChecked() && !options.get(i).getText().toString().equals("")) {
+                            correctAnswer = answerOptions.size();
+                            answerOptions.add(options.get(i).getText().toString());
+                        } else if (!options.get(i).getText().toString().equals("")) {
+                            answerOptions.add(options.get(i).getText().toString());
+                        }
+                    }
+                    String questionText = question.getText().toString();
+                    quizData.set(position, new QuizModel(questionText, answerOptions, correctAnswer));
+                    notifyDataSetChanged();
+                }
+
+            }
+        });
+        dialog.show();
+
+    }
+
+    private void checkButton(ArrayList<RadioButton> buttons, ArrayList<EditText> options, int id, Context context) {
+        for (RadioButton button : buttons) {
+            if (button.getId() == id) {
+                int index = buttons.indexOf(button);
+                if (options.get(index).getText().toString().equals("")) {
+                    Toast.makeText(context, "Enter a valid option before marking it as answer", Toast.LENGTH_LONG).show();
+                    button.setChecked(false);
+                    return;
+                } else {
+                    button.setChecked(true);
+                }
+            } else {
+                button.setChecked(false);
+            }
+        }
+    }
+
+    private int getCheckedAnswer(ArrayList<RadioButton> buttons) {
+        for (int i = 0; i < buttons.size(); i++) {
+            if (buttons.get(i).isChecked()) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public class Holder {
@@ -112,5 +250,7 @@ public class QuizAdapter extends BaseAdapter {
         TextViewPlus questionIcon;
         ArrayList<TextViewPlus> options;
         LinearLayout quizOptionsBox;
+        Button delete;
+        Button edit;
     }
 }
