4c6c50c1e3927ceab8fda4673bd09a0c63cb6dd4
Abhishek Batra <abhishekbatra1993@gmail.com>
2015-06-11 02:27:21 +0530

    Added Zip logic

--- source-code/app/src/main/java/org/buildmlearn/toolkit/utilities/FileUtils.java
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/utilities/FileUtils.java
@@ -9,10 +9,13 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -218,5 +221,77 @@ public class FileUtils {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public static void zipFolder(String directoryToZipPath ) throws IOException {
+        File directoryToZip = new File(directoryToZipPath);
+
+        List<File> fileList = new ArrayList<File>();
+        System.out.println("---Getting references to all files in: " + directoryToZip.getCanonicalPath());
+        getAllFiles(directoryToZip, fileList);
+        System.out.println("---Creating zip file");
+        writeZipFile(directoryToZip, fileList);
+        System.out.println("---Done");
+    }
+
+    public static void getAllFiles(File dir, List<File> fileList) {
+        try {
+            File[] files = dir.listFiles();
+            for (File file : files) {
+                fileList.add(file);
+                if (file.isDirectory()) {
+                    System.out.println("directory:" + file.getCanonicalPath());
+                    getAllFiles(file, fileList);
+                } else {
+                    System.out.println("     file:" + file.getCanonicalPath());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeZipFile(File directoryToZip, List<File> fileList) {
+
+        try {
+            FileOutputStream fos = new FileOutputStream(directoryToZip.getAbsolutePath() + ".zip");
+            ZipOutputStream zos = new ZipOutputStream(fos);
+
+            for (File file : fileList) {
+                if (!file.isDirectory()) { // we only zip files, not directories
+                    addToZip(directoryToZip, file, zos);
+                }
+            }
+
+            zos.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addToZip(File directoryToZip, File file, ZipOutputStream zos) throws FileNotFoundException,
+            IOException {
+
+        FileInputStream fis = new FileInputStream(file);
+
+        // we want the zipEntry's path to be a relative path that is relative
+        // to the directory being zipped, so chop off the rest of the path
+        String zipFilePath = file.getCanonicalPath().substring(directoryToZip.getCanonicalPath().length() + 1,
+                file.getCanonicalPath().length());
+        System.out.println("Writing '" + zipFilePath + "' to zip file");
+        ZipEntry zipEntry = new ZipEntry(zipFilePath);
+        zos.putNextEntry(zipEntry);
+
+        byte[] bytes = new byte[1024];
+        int length;
+        while ((length = fis.read(bytes)) >= 0) {
+            zos.write(bytes, 0, length);
+        }
+
+        zos.closeEntry();
+        fis.close();
     }
 }
