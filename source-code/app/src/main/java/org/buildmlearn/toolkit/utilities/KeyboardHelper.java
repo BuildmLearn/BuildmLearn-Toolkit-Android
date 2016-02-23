c0db69e2a4990d81b68b0dd3c687e135c2e0c4a6
Anupam Das <anupam.das.bwn@gmail.com>
2016-02-24 04:50:16 +0530

    Added #41 , Personalized Keyboard feature

---
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/utilities/KeyboardHelper.java
@@ -0,0 +1,27 @@
+package org.buildmlearn.toolkit.utilities;
+
+import android.app.Activity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * User : opticod(Anupam Das)
+ * Date : 24/2/16.
+ */
+public class KeyboardHelper {
+    public static void hideKeyboard(final Activity activity, View view) {
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                View view = activity.getCurrentFocus();
+                if (view != null) {
+                    InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+                return false;
+            }
+        });
+    }
+}
+
