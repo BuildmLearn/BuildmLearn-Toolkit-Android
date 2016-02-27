bed3ed2dc97db90a98c362805f31a0f3d46c3d02
Anupam Das <anupam.das.bwn@gmail.com>
2016-02-27 19:15:53 +0530

    Project Deletion Feature Added

--- source-code/app/src/main/java/org/buildmlearn/toolkit/activity/TemplateActivity.java
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/activity/TemplateActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
