60448507c4402e09820d7061aa2654875580354e
Shreyas Patel <shreyaspatel29@gmail.com>
2016-03-26 18:46:50 +0530

    Fixed Issue: #23, #24

--- source-code/app/src/main/java/org/buildmlearn/toolkit/activity/TemplateEditor.java
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/activity/TemplateEditor.java
@@ -243,6 +243,11 @@ public class TemplateEditor extends AppCompatActivity {
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        if (showTemplateSelectedMenu) {
+            getMenuInflater().inflate(R.menu.menu_template_item_selected, menu);
+        } else {
+            getMenuInflater().inflate(R.menu.menu_template_editor, menu);
+        }
         return true;
     }
 
@@ -252,12 +257,6 @@ public class TemplateEditor extends AppCompatActivity {
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         Log.d(TAG, "onPrepareOptionsMenu");
-        menu.clear();
-        if (showTemplateSelectedMenu) {
-            getMenuInflater().inflate(R.menu.menu_template_item_selected, menu);
-        } else {
-            getMenuInflater().inflate(R.menu.menu_template_editor, menu);
-        }
         return true;
     }
 
