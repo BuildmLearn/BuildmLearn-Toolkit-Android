fd8b3b73a419a6b4c08c5afa1249fa814aa87a3d
Anupam Das <anupam.das.bwn@gmail.com>
2016-05-21 04:38:16 +0530

    Added Simulator's Constants

---
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/videoCollectionTemplate/Constants.java
@@ -0,0 +1,22 @@
+package org.buildmlearn.toolkit.videoCollectionTemplate;
+
+import org.buildmlearn.toolkit.videoCollectionTemplate.data.VideoContract;
+
+/**
+ * Created by Anupam (opticod) on 13/5/16.
+ */
+public class Constants {
+    public static final String[] VIDEO_COLUMNS = {
+            VideoContract.Videos.TABLE_NAME + "." + VideoContract.Videos._ID,
+            VideoContract.Videos.TITLE,
+            VideoContract.Videos.DESCRIPTION,
+            VideoContract.Videos.LINK,
+            VideoContract.Videos.THUMBNAIL_URL
+    };
+    public static final int COL_ID = 0;
+    public static final int COL_TITLE = 1;
+    public static final int COL_DESCRIPTION = 2;
+    public static final int COL_LINK = 3;
+    public static final int COL_THUMBNAIL_URL = 4;
+    public static String XMLFileName = "video_content.xml";
+}
