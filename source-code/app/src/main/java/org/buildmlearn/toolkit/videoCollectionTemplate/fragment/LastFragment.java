48dd551291fb28de00c7d76226a4e46b6aba61e4
Anupam Das <anupam.das.bwn@gmail.com>
2016-05-21 04:39:27 +0530

    Added Simulator's LastFragment.java

---
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/videoCollectionTemplate/fragment/LastFragment.java
@@ -0,0 +1,57 @@
+package org.buildmlearn.toolkit.videoCollectionTemplate.fragment;
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.buildmlearn.toolkit.R;
+
+/**
+ * Created by Anupam (opticod) on 21/5/16.
+ */
+public class LastFragment extends Fragment {
+
+    public LastFragment() {
+
+    }
+
+    public static Fragment newInstance() {
+        LastFragment fragment = new LastFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_last_video, container, false);
+//
+//        getActivity().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home_white_24dp);
+
+        rootView.findViewById(R.id.restart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle arguments = new Bundle();
+                arguments.putString(Intent.EXTRA_TEXT, "1");
+
+                Fragment frag = DetailActivityFragment.newInstance();
+                frag.setArguments(arguments);
+                getActivity().getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                getActivity().getSupportFragmentManager().beginTransaction().replace(((ViewGroup) getView().getParent()).getId(), frag).addToBackStack(null).commit();
+            }
+        });
+
+        rootView.findViewById(R.id.exit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+
+        return rootView;
+    }
+}
