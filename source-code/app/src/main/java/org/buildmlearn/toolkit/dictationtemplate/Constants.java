875f21ed04e85e8af866a785ec746ac3be3c2323
Anupam Das <anupam.das.bwn@gmail.com>
2016-07-10 22:07:26 +0530

    Added dictation simulator's Utilities + constants

---
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/dictationtemplate/Constants.java
@@ -0,0 +1,20 @@
+package org.buildmlearn.toolkit.dictationtemplate;
+
+import org.buildmlearn.toolkit.dictationtemplate.data.DictContract;
+
+/**
+ * Created by Anupam (opticod) on 10/7/16.
+ */
+public class Constants {
+    public static final String[] DICT_COLUMNS = {
+            DictContract.Dict.TABLE_NAME + "." + DictContract.Dict._ID,
+            DictContract.Dict.TITLE,
+            DictContract.Dict.PASSAGE
+    };
+    public static final int COL_ID = 0;
+    public static final int COL_TITLE = 1;
+    public static final int COL_PASSAGE = 2;
+    public static final String firstrun = "firstRun";
+    public static final String passage = "PASSAGE";
+    public static String XMLFileName = "dictation_content.xml";
+}
