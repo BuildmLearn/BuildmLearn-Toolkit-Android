b206630cf7b7985c91bf140abc18179ec1246196
Anupam Das <anupam.das.bwn@gmail.com>
2016-08-07 19:38:20 +0530

    Removed old simulators java files

---
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/learnspelling/Constants.java
@@ -0,0 +1,14 @@
+package org.buildmlearn.toolkit.learnspelling;
+
+/**
+ * Created by Anupam (opticod) on 31/5/16.
+ */
+public class Constants {
+    public static final String firstrun = "firstRun";
+    public static final int COL_WORD = 1;
+    public static final int COL_MEANING = 2;
+    public static final int COL_ANSWERED = 3;
+    public static final int COL_ATTEMPTED = 4;
+    public static String XMLFileName = "spelling_content.xml";
+
+}
