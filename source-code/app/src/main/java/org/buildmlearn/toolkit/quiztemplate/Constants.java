6b130ffd6fb1b631ba24684bf531374fcb7d4b5b
Anupam Das <anupam.das.bwn@gmail.com>
2016-08-14 00:47:36 +0530

    Added all fragments for quiz_template + constants

---
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/quiztemplate/Constants.java
@@ -0,0 +1,18 @@
+package org.buildmlearn.toolkit.quiztemplate;
+
+/**
+ * Created by Anupam (opticod) on 14/8/16.
+ */
+public class Constants {
+    public static final String firstrun = "firstRun";
+    public static final int COL_QUESTION = 1;
+    public static final int COL_OPTION_1 = 2;
+    public static final int COL_OPTION_2 = 3;
+    public static final int COL_OPTION_3 = 4;
+    public static final int COL_OPTION_4 = 5;
+    public static final int COL_CORRECT_ANSWER = 6;
+    public static final int COL_ANSWERED = 7;
+    public static final int COL_ATTEMPTED = 8;
+    public static String XMLFileName = "quiz_content.xml";
+
+}
