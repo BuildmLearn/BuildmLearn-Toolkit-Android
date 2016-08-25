caad4771098715a9b210e8509db019cbb57ad6c0
Teja Vojjala <iit2014086@iiita.ac.in>
2016-08-25 20:59:37 +0530

    Update LoadApkFragment.java

--- source-code/app/src/main/java/org/buildmlearn/toolkit/fragment/LoadApkFragment.java
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/fragment/LoadApkFragment.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.content.pm.PackageInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
