d6838ab01b7cc3addb5c3b024a4d966c2ac99bfb
octacode <allblue.shasha@gmail.com>
2016-12-12 22:17:11 +0530

    Enhancing the delete feature.

--- source-code/app/src/main/java/org/buildmlearn/toolkit/fragment/LoadProjectFragment.java
+++ source-code/app/src/main/java/org/buildmlearn/toolkit/fragment/LoadProjectFragment.java
@@ -479,7 +479,10 @@ public class LoadProjectFragment extends Fragment implements AbsListView.OnItemC
             }
         }
         if(deleted)
-            Toast.makeText(activity, "Project Successfully Deleted!", Toast.LENGTH_SHORT).show();
+            if(selectedPositions.size()==1)
+                Toast.makeText(activity,"Project Successfully Deleted", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(activity,selectedPositions.size()+" Projects Successfully Deleted", Toast.LENGTH_SHORT).show();
         else
             Toast.makeText(activity, "Project Deletion Failed!", Toast.LENGTH_SHORT).show();
     }
